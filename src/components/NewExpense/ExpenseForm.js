b1e443f5264906e7757d25fd4975854f92216836
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2021-11-06 21:36:18 +0300

    Initial commit

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const titleChangeEvent = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredTitle: event.target.value };
+    });
+  };
+  const amountChangeEvent = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: event.target.value };
+    });
+  };
+  const dateChangeEvent = (event) => {
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: event.target.value };
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setUserInput({
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={userInput.enteredTitle}
+            onChange={titleChangeEvent}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="text"
+            value={userInput.enteredAmount}
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeEvent}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            value={userInput.enteredDate}
+            min="2019-01-01"
+            step="2022-01-01"
+            onChange={dateChangeEvent}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
